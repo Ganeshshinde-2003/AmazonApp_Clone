458c96360aed4d19e6ee4b942c00f23bf2a098bd
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-07-15 09:21:34 +0530

    Feature: Finished the signup

--- lib/router.dart
+++ lib/router.dart
@@ -2,15 +2,16 @@ import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
-  switch(routeSettings.name) {
+  switch (routeSettings.name) {
     case AuthScreen.routeName:
-      return MaterialPageRoute(builder: (_) => const AuthScreen(), settings: routeSettings);
-      break;
+      return MaterialPageRoute(
+          builder: (_) => const AuthScreen(), settings: routeSettings);
     default:
-      return MaterialPageRoute(builder: (_) => const Scaffold(
-        body: Center(
-          child: Text("Page Not Found"),
-        ),
-      ));
+      return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+                body: Center(
+                  child: Text("Page Not Found"),
+                ),
+              ));
   }
 }
