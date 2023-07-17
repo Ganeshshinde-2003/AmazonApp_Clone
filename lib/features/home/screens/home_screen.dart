50393c3ec96431bd5d0b5155d79a744f03900341
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-07-17 13:42:11 +0530

    Feature: Navbar of the homepage is created

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:amazon_clone/features/home/widgets/address_box.dart';
-import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 import '../../../contants/globlal_varible.dart';
 
